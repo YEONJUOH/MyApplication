7c8c09e9fb4cbb6dec9fdb05ce591cffad3532ea
YEONJUOH <eorol2@naver.com>
2016-10-21 19:03:57 +0900

    #첫 푸쉬

---
+++ src/main/java/com/example/hong3/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hong3.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+       // return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+    //http://loveiskey.tistory.com/176
+    //http://dev-daddy.com/8
+    //http://sumi3360.blogspot.kr/2015/07/android-action-item-icon.html
+}
